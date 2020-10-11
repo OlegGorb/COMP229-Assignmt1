b10450ecdd2f1c0ed401ad7485c702c4ce6e6230
OlegGorb <72626535+OlegGorb@users.noreply.github.com>
2020-10-10 23:51:07 -0400

    Assignment1

---
+++ public/scripts/app.js
@@ -0,0 +1,11 @@
+// IIFE - - Immediately invoked function exression
+/* Assignment 1 - Oleg Gorbunov - 301093263 - 10.10.2020 */
+
+(function() {
+
+    function start() {
+        console.log("App started...")
+    }
+
+    window.addEventListener("load", start);
+})();
